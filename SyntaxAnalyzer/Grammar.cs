28238b07bd34cdef9d2fcdf86f7ba248d27363fe
Emanon <fqqdc@qq.com>
2022-01-26 12:11:23 +0800

    update

---
+++ SyntaxAnalyzer/Grammar.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyntaxAnalyzer
+{
+    public class Grammar
+    {
+        public Grammar(IEnumerable<Production> allProduction, NonTerminal startNonTerminal)
+        {
+            var symbols = allProduction.SelectMany(p => p.Right.Append(p.Left)).Distinct();
+            _Vn.UnionWith(symbols.Where(s => s is NonTerminal).Cast<NonTerminal>());
+            _Vt.UnionWith(symbols.Where(s => s is Terminal).Cast<Terminal>());
+            _P.UnionWith(allProduction);
+            S = startNonTerminal;
+        }
+
+        private Dictionary<NonTerminal, HashSet<Terminal>> mapFirst = new();
+        private Dictionary<NonTerminal, HashSet<Terminal>> mapFollow = new();
+        private HashSet<NonTerminal> nullableSet = new();
+        private HashSet<Terminal> _Vt = new();
+        private HashSet<NonTerminal> _Vn = new();
+        private HashSet<Production> _P = new();
+
+        public IEnumerable<Terminal> Vt { get => _Vt.AsEnumerable(); }
+        public IEnumerable<NonTerminal> Vn { get => _Vn.AsEnumerable(); }
+        public IEnumerable<Production> P { get => _P.AsEnumerable(); }
+        public NonTerminal S { get; private set; }
+
+
+
+        #region ToString()
+        const string PRE = "    ";
+
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append("Grammar").AppendLine();
+            VtToString(builder);
+            VnToString(builder);
+            PToString(builder);
+            SToString(builder);
+            return builder.ToString();
+        }
+
+        public string ToFullString()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append(ToString());
+            return builder.ToString();
+        }
+
+        private void VtToString(StringBuilder builder)
+        {
+            builder.Append(PRE).Append("Terminals : {");
+            foreach (var t in Vt)
+            {
+                builder.Append($" {t},");
+            }
+            builder.Length -= 1;
+            builder.Append(" }");
+            builder.AppendLine();
+        }
+
+        private void VnToString(StringBuilder builder)
+        {
+            builder.Append(PRE).Append("NonTerminal : {");
+            foreach (var n in Vn)
+            {
+                builder.Append($" {n},");
+            }
+            builder.Length -= 1;
+            builder.Append(" }");
+            builder.AppendLine();
+        }
+
+        private void PToString(StringBuilder builder)
+        {
+            builder.Append(PRE).Append("Productions :").AppendLine();
+            builder.Append(PRE).Append("{").AppendLine();
+            foreach (var p in P.OrderBy(p => sortedList.IndexOf(p.Left))
+                .ThenByDescending(p => p.Right.Length))
+            {
+                builder.Append(PRE).Append(PRE).Append(p).AppendLine();
+            }
+            builder.Append(PRE).Append("}").AppendLine();
+        }
+
+        private void SToString(StringBuilder builder)
+        {
+            builder.Append(PRE).Append($"START : {S}").AppendLine();
+        }
+
+
+
+        #endregion
+    }
+}
---
+++ SyntaxAnalyzer/Symbol.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+namespace SyntaxAnalyzer
+{
+    public abstract record Symbol(string Name)
+    {
+        public override string ToString()
+        {
+            return $"{Name}";
+        }
+    }
+    public record Terminal(string Name) : Symbol(Name)
+    {
+        public static Terminal Epsilon { get; private set; } = new("\"eps\"");
+    }
+    public record NonTerminal(string Name) : Symbol(Name)
+    {            
+    }
+}
